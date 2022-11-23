d803cf4988e302ba003b5b27c6a365ea895da397
Darya Suravtsova <darblnkapm@gmail.com>
2022-11-24 00:58:14 +0300

    refactor: devide script.js on functions

--- songbird/script.js
+++ songbird/script.js
@@ -2,58 +2,15 @@ import birdsData from './birds.js'
 
 let question = 0
 let sum = 0
+let currentAudio
 
-async function createPage (question = 0, sum = 0) {
-  console.log(question, sum)
-  let nav = document.querySelectorAll('.questions p')
-  if (question > 0)
-    nav[question - 1].style.transform = 'scale(1)'
-  nav[question].style.transform = 'scale(1.4)'
-
-  let birdImg = document.getElementsByClassName('image')[0]
-  let birdName = document.querySelector('.question h2')
-
-  birdImg.src = './images/unknown_bird.jpg'
-  birdName.innerHTML = 'Какая птица?'
-
-  let birdImg_2 = document.getElementsByClassName('image-2')[0]
-  let birdName_2 = document.querySelector('.bird h2')
-  let birdSpecies = document.querySelector('.bird h3')
-
-  birdImg_2.src = './images/unknown_bird.jpg'
-  birdName_2.innerHTML = 'Какая птица?'
-  birdSpecies.innerHTML = ''
-  
-  let aboutBird = document.querySelector('.about')
-  aboutBird.innerHTML = ''
-
-  let range_2 = document.getElementsByClassName('range-time-2')[0]
-  let timePicker_2 = document.getElementsByClassName('timer-2')[0]
-  let playImg_2 = document.getElementsByClassName("play-audio-2")[0]
-
-  let markers = Array.from(document.getElementsByClassName('marker'))
-  markers.forEach((marker) => {
-    console.log(marker)
-    marker.style.backgroundColor = "rgba(0, 0, 0, 0.449)"
-  })
-
-  let answer = {}
-
-  let random = Math.floor(Math.random() * 6) + 1
-  console.log(random)
-  birdsData[question].forEach((bird) => {
-    if (bird.id == random)
-      answer = bird
-  })
-
-  console.log(answer)
-
-  let src_1 = answer.audio
-  let audio = new Audio(src_1)
+function createFirstPlayer (bird) {
+  let src = bird.audio
+  let audio = new Audio(src)
 
   let range = document.getElementsByClassName('range-time')[0]
   let timePicker = document.getElementsByClassName('timer')[0]
-  let playImg = document.getElementsByClassName("play-audio")[0]
+  let playImg = document.getElementsByClassName('play-audio')[0]
 
   audio.addEventListener('loadedmetadata', () => {
     range.max = Math.floor(audio.duration)
@@ -61,17 +18,23 @@ async function createPage (question = 0, sum = 0) {
     range.step = 0.1
   })
 
-  playImg.onclick = function (){
+  playImg.onclick = () => {
+    if (currentAudio && currentAudio != audio) {
+      currentAudio.pause()
+    }
     if (audio.paused) {
       audio.play()
-      this.src = "./images/play.png"
+      currentAudio = audio
+      playImg.src = "./images/play.png"
     } else {
       audio.pause()
-      this.src = "./images/pause.png"
     }
   }
-  audio.addEventListener('ended', function () {
+  audio.addEventListener('ended', () => {
     audio.currentTime = 0
+    playImg.src = "./images/pause.png"
+  })
+  audio.addEventListener('pause', () => {
     playImg.src = "./images/pause.png"
   })
 
@@ -106,122 +69,208 @@ async function createPage (question = 0, sum = 0) {
       volume.src = './images/audio.png'
     }
   })
-
-  let answers = document.getElementsByClassName('ans')
-
-  let k = 0
-  birdsData[question].forEach((bird) => {
-    answers[k].innerHTML = bird.name
-    k++
-  })
-
-  let names = []
-  let h = 0
-  birdsData[question].forEach((bird) => {
-    names[h] = bird.name
-    h++
-  })
-
-  let buttonNext = document.querySelector('.next-level')
-  buttonNext.disabled = true
-
-  let res = 5
-  let score = document.querySelector('.score span')
-  Array.from(answers).forEach((ans) => {
-    ans.parentElement.addEventListener('click', () => {
-      createSecondAudio(birdsData[question][names.indexOf(ans.innerHTML)])
-      if (answer.name == ans.innerHTML) {
-        markers[answer.id - 1].style.backgroundColor = "rgb(0, 103, 69)"
-        birdImg.src = answer.image
-        birdName.innerHTML = answer.name
-        sum = sum + res
-        score.innerHTML = sum
-        buttonNext.disabled = false
-        buttonNext.addEventListener('click', () => {
-          if (question != 5) {
-            question++
-            playImg.src = './images/pause.png'
-            range.value = 0
-            TimeRanges.innerHTML = '00:00:00'
-            audio.pause()  
-            range_2.value = 0
-            timePicker_2.innerHTML = '00:00:00'
-            playImg_2.src = './images/pause.png'
-            createPage(question, sum)
-          }
-          else {
-            congratulations(sum)
-          }
-        })
-      }
-      else {
-        if (markers[answer.id - 1].style.backgroundColor != "rgb(0, 103, 69)") 
-          markers[names.indexOf(ans.innerHTML)].style.backgroundColor = "red"
-      }
-      res--
-    })
-  })
 }
 
-function createSecondAudio(bird) {
-  let src = bird.audio
+function clearFirstPlayer () {
+  let range = document.getElementsByClassName('range-time')[0]
+  let timePicker = document.getElementsByClassName('timer')[0]
+  let playImg = document.getElementsByClassName("play-audio")[0]
+
+  range.value = 0
+  timePicker = "00:00:00"
+  playImg.src = './images/pause.png'
+}
+
+async function createSecondPlayer (bird) {
+  clearSecondPlayer()
+
+  let src = await bird.audio
   let audio = new Audio(src)
 
-  let birdImg = document.getElementsByClassName('image-2')[0]
+  let birdImg = document.querySelector('.image-2')
   let birdName = document.querySelector('.bird h2')
   let birdSpecies = document.querySelector('.bird h3')
+  let aboutBird = document.querySelector('.about')
 
-  let range_2 = document.getElementsByClassName('range-time-2')[0]
-  let timePicker_2 = document.getElementsByClassName('timer-2')[0]
-  let playImg_2 = document.getElementsByClassName("play-audio-2")[0]
-
+  let range = document.getElementsByClassName('range-time-2')[0]
+  let timePicker = document.getElementsByClassName('timer-2')[0]
+  let playImg = document.getElementsByClassName("play-audio-2")[0]
+  
   birdImg.src = bird.image
   birdName.innerHTML = bird.name
   birdSpecies.innerHTML = bird.species
+  aboutBird.innerHTML = bird.description
 
   audio.addEventListener('loadedmetadata', () => {
-    range_2.max = Math.floor(audio.duration)
-    range_2.min = 0
-    range_2.step = 0.1
+    range.max = Math.floor(audio.duration)
+    range.min = 0
+    range.step = 0.1
   })
 
-  playImg_2.onclick = function (){
+  playImg.onclick = function () {
+    if (currentAudio && currentAudio != audio) {
+      currentAudio.pause()
+    }
     if (audio.paused) {
       audio.play()
-      this.src = "./images/play.png"
+      currentAudio = audio
+      currentAudio.classList.add('active')
+      playImg.src = "./images/play.png"
     } else {
       audio.pause()
-      this.src = "./images/pause.png"
     }
   }
   audio.addEventListener('ended', function () {
     audio.currentTime = 0
-    playImg_2.src = "./images/pause.png"
+    playImg.src = "./images/pause.png"
+  })
+  audio.addEventListener('pause', function () {
+    playImg.src = "./images/pause.png"
   })
 
   audio.addEventListener('timeupdate', () => {
     let date = new Date(0)
     date.setSeconds(audio.currentTime)
-    timePicker_2.innerHTML = date.toISOString().substring(11, 19)
-    range_2.value = audio.currentTime
+    timePicker.innerHTML = date.toISOString().substring(11, 19)
+    range.value = audio.currentTime
   })
 
-  range_2.addEventListener('input', () => {         
-    audio.currentTime = range_2.value
+  range.addEventListener('input', () => {         
+    audio.currentTime = range.value
   })
-
-  let aboutBird = document.querySelector('.about')
-  aboutBird.innerHTML = bird.description
 }
 
-createPage(question)
+function clearSecondPlayer () {
+  let range = document.querySelector('.range-time-2')
+  let timePicker = document.querySelector('.timer-2')
+  let playImg = document.querySelector('.play-audio-2')
+
+  range.value = 0
+  timePicker.innerHTML = "00:00:00"
+  playImg.src = './images/pause.png'
+
+  currentAudio && currentAudio.classList.contains('active') ? currentAudio.pause() && currentAudio.classList.remove('active') : true
+}
+
+function clearBullets () {
+  let markers = document.getElementsByClassName('marker')
+
+  Array.from(markers).forEach((marker) => {
+    marker.style.backgroundColor = "rgba(0, 0, 0, 0.449)"
+  })
+}
+
+function createAnswer (question) {
+  let answer = {}
+
+  let random = Math.floor(Math.random() * 6)
+  answer = birdsData[question][random]
+
+  return answer
+}
+
+function nav (question) {
+  let nav = document.querySelectorAll('.questions p')
+  if (question > 0)
+    nav[question - 1].style.transform = 'scale(1)'
+  nav[question].style.transform = 'scale(1.2)'
+}
+
+function createVariants (question) {
+  let box = document.querySelector('.answers')
+  box.innerHTML = ''
+
+  for (let i = 0; i < 6; i++) {
+    let variant = document.createElement('div')
+
+    let marker = document.createElement('div')
+    marker.classList.add('marker')
+    let paragraph = document.createElement('p')
+    paragraph.innerHTML = birdsData[question][i].name
+    paragraph.classList.add('ans')
+
+    variant.append(marker, paragraph)
+    box.append(variant)
+  }
+}
+
+let buttonNext = document.querySelector('.next-level')
+
+buttonNext.addEventListener('click', () => {
+  let s = document.querySelector('.score span')
+  if (question != 5) {
+    question++
+    clearBullets()
+    clearFirstPlayer()
+    clearSecondPlayer()
+    clearBullets()
+    createPage(question, parseInt(s.innerHTML))
+  }
+  else {
+    congratulations(parseInt(s.innerHTML))
+  }
+})
+
+let res = 5
+function createPage(question, sum) {
+  console.log(sum)
+  nav(question)
+  createVariants (question)
+
+  let answer = createAnswer(question)
+  console.log(answer)
+
+  createFirstPlayer(answer)
+
+  buttonNext.setAttribute('disabled', 'disabled')
+
+  let variants = document.getElementsByClassName('ans')
+
+  let k = 0
+  birdsData[question].forEach((bird) => {
+    variants[k].innerHTML = ''
+    variants[k].innerHTML = bird.name
+    k++
+  })
+
+  let markers = document.getElementsByClassName('marker')
+  let birdName = document.querySelector('.question h2')
+  let birdImg = document.querySelector('.image')
+
+  let score = document.querySelector('.score span')
+
+  res = 5 
+  console.log(res)
+  Array.from(variants).forEach((variant, i) => {
+    variant.parentElement.addEventListener('click', () => {
+      createSecondPlayer(birdsData[question][i])
+      if (answer.name != variant.innerHTML) {
+        if (markers[i].style.backgroundColor != 'red') {
+          if (Array.from(markers).filter((marker) => (marker.style.backgroundColor == 'green')).length == 0)
+            markers[i].style.backgroundColor = 'red'
+            res--
+        }
+      }
+      else {        
+        markers[i].style.backgroundColor = 'green'
+        birdImg.src = answer.image
+        birdName.innerHTML = answer.name
+        sum = sum + res
+        score.innerHTML = sum
+        buttonNext.disabled = false
+        currentAudio?.pause()
+      }
+      console.log(res)
+    })
+  })
+}
 
 function congratulations(sum) {
   let main = document.querySelector('.quize')
   let nav = document.querySelectorAll('.questions p')
 
   main.innerHTML = ''
-  nav = ''
+  nav.innerHTML = ''
   
   let paragraph = document.createElement('h3')
   paragraph.innerHTML = 'Поздравляем, вы набрали ' + sum + ' баллов!'
@@ -230,3 +279,5 @@ function congratulations(sum) {
 
   main.append(paragraph)
 }
+
+createPage(question, sum)
